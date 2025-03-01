10c1a3ca58b3e0b66984d697df476de254eb998b
André Souza <contatosouzaandre@gmail.com>
2025-03-01 18:14:32 -0300

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -9,3 +9,17 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+function applyTheme() {
+    const hour = new Date().getHours();
+    const body = document.body;
+    if (hour >= 6 && hour < 18) {
+        body.classList.add('light-theme');
+        body.classList.remove('dark-theme');
+    } else {
+        body.classList.add('dark-theme');
+        body.classList.remove('light-theme');
+    }
+}
+// Chame a função applyTheme() para aplicar o tema inicial
+window.addEventListener('load', applyTheme);
